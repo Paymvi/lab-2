1b1012da29c5974bf09e2a6bf27ab8ff25be8555
Chelsea Alysson Ongjoco <chelsalysongjoco@yahoo.com>
2025-10-07 15:31:21 -0400

    Added basic map

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,38 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { MapContainer, TileLayer } from 'react-leaflet';
+// MapContainer: initializes and manages the Leaflet map
+ 
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    <div style={{ height: '100vh', width: '100vw'}}>
+    
+        {/* This is where the map lives */}
+        <MapContainer
+          center={[43.03859371008897, -71.44920665422427]} // SNHU coordinates
+          zoom={15}
+          style={{ height: '100%', width: '100%'}}
+        >
+
+        {/* TileLayer defines the source of the map imagery */}
+        {/* We are using OpenStreetMap */}
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        
+
+      </MapContainer>
+    
+    </div>
+  );
 }
 
 export default App
