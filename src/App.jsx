b81409aa29013b8ad1d84928914edcbce86f9dfc
Chelsea Alysson Ongjoco <chelsalysongjoco@yahoo.com>
2025-10-07 16:23:29 -0400

    Added marker feature

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,30 @@ import './App.css'
 import { MapContainer, TileLayer } from 'react-leaflet';
 // MapContainer: initializes and manages the Leaflet map
  
+import {  Marker, Popup, useMapEvents } from 'react-leaflet';
 
+// Note... clickhandler is outside app
+function ClickHandler( { onMapClick }){
+    useMapEvents({
 
+      // e stands for "event" and is the convention in JavaScript
+      // We are taking the latitude and longitude of the click and passing it to the "handleMapClick" function
+      click(e) {
+        onMapClick(e.latlng);
+      }
+    });
+    return null;
+  }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [locations, setLocations] = useState([]);
+
+  // Here we add to the list of locations
+  const handleMapClick = (latlng) => {
+    setLocations((prev) => [...prev, {latlng}])
+  };
+
+
 
   return (
     
@@ -19,7 +38,7 @@ function App() {
         {/* This is where the map lives */}
         <MapContainer
           center={[43.03859371008897, -71.44920665422427]} // SNHU coordinates
-          zoom={15}
+          zoom={15.5}
           style={{ height: '100%', width: '100%'}}
         >
 
@@ -28,7 +47,17 @@ function App() {
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          
+          detectRetina={true} // << UNDERRATED: Makes the map higher resolution
+
         />
+
+        <ClickHandler onMapClick={handleMapClick} />
+        {locations.map((loc, i) => (
+          <Marker key={i} position={loc.latlng}>
+            
+          </Marker>
+        ))}
         
 
       </MapContainer>
